89a7db5d7eca3147cbec697980bda3531a89fc04
Alastor Wu <alastor0325@gmail.com>
2026-06-27 12:03:50 -0700

    make "Go back to top" jump instantly
    The back-to-top button used a manual animation scrolling -50px every
    10ms, which crawled on long articles. Replace it with an immediate
    window.scrollTo(0, 0).

--- src/js/main/recommendation.js
+++ src/js/main/recommendation.js
@@ -29,12 +29,7 @@
         }, false);
     }
 
-    var timeOut;
     function scrollToTop() {
-        if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
-            window.scrollBy(0, -50);
-            timeOut = setTimeout(scrollToTop, 10);
-        }
-        else clearTimeout(timeOut);
+        window.scrollTo(0, 0);
     }
 })();
